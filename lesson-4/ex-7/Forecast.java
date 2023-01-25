6f80238dcf5601316c8356f98a3a04b70fb7a0c1
kamilp999 <115872107+kamilp999@users.noreply.github.com>
2023-01-25 17:38:01 +0100

    Delete lesson-4/ex-7 directory

--- lesson-4/ex-7/Forecast.java
+++
@@ -1,9 +0,0 @@
-public interface Forecast {
-    public float nowTemperature();
-    final class Fake_1 implements Forecast {
-        @Override
-        public float nowTemperature(){
-            return 20.1f;
-        }
-    }
-}
--- lesson-4/ex-7/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main
-{
-    public static void main(String[] args) {
-        Forecast forecast = new Forecast.Fake_1();
-
-        System.out.println(forecast.nowTemperature());
-    }
-}
